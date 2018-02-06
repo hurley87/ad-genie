f7fec1c7b11fd9f6041eaec877faf8dc2f975752
Carey Costa <careyxcosta@gmail.com>
2018-02-05 19:17:07 -0500

    created styles

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -9,7 +9,7 @@ const Index = () => (
       src="https://s3.us-east-2.amazonaws.com/snapmortgages/cool.jpg"
       alt="Clever Beagle"
     />
-    <h3>Prospecting is tedious, time-consuming and inefficient.</h3>
+    <h3>Prospecting is tedious, and inefficient.</h3>
     <p>We find your prospects, and drive qualified interest right to your inbox. You step in to close the deal.</p>
     <div>
       <Button href="/signup">Get Started</Button>
