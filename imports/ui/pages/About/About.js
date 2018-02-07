799ba2d5e591764240caa5062d48a88fe6fdccd4
Carey Costa <careyxcosta@gmail.com>
2018-02-07 14:47:02 -0500

    vertically aligned content of About to center

--- imports/ui/pages/About/About.js
+++ imports/ui/pages/About/About.js
@@ -4,19 +4,21 @@ import './About.scss';
 
 const About = () => (
   <div className="About">
-    <h3>How It Works</h3>
-    <p>
-      <span className="list-num">1. </span>
-      Tell us about your <b>properties</b> and <b>ideal customers</b>
-    </p>
-    <p>
-      <span className="list-num">2. </span>
-      We engage in <b>conversations</b> with your <b>ideal customers</b>
-    </p>
-    <p>
-      <span className="list-num">3. </span>
-      They <b>respond with interest</b> and you <b>close deals</b>
-    </p>
+    <div className="contentWrapper">
+      <h3>How It Works</h3>
+      <p>
+        <span className="list-num">1. </span>
+        Tell us about your <b>properties</b> and <b>ideal customers</b>
+      </p>
+      <p>
+        <span className="list-num">2. </span>
+        We engage in <b>conversations</b> with your <b>ideal customers</b>
+      </p>
+      <p>
+        <span className="list-num">3. </span>
+        They <b>respond with interest</b> and you <b>close deals</b>
+      </p>
+    </div>
   </div>
 );
 
