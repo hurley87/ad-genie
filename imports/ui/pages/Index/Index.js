2797e20bec74ee6e7dbe97fd241826f76a4c30f5
Carey Costa <careyxcosta@gmail.com>
2018-02-07 16:07:34 -0500

    left-aligned Index heading, made image inline-block

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -5,14 +5,16 @@ import './Index.scss';
 
 const Index = () => (
   <div className="Index">
-    <img
-      src="https://s3.us-east-2.amazonaws.com/snapmortgages/cool.jpg"
-      alt="Clever Beagle"
-    />
-    <h3>Prospecting is tedious, and inefficient.</h3>
-    <p>We find your prospects, and drive qualified interest right to your inbox. You step in to close the deal.</p>
-    <div>
-      <Button href="/signup">Get Started</Button>
+    <div className="contentWrapper">
+      <h3>Prospecting is tedious, and inefficient.</h3>
+      <img
+        src="https://s3.us-east-2.amazonaws.com/snapmortgages/cool.jpg"
+        alt="Clever Beagle"
+      />
+      <p>We find your prospects, and drive qualified interest right to your inbox. You step in to close the deal.</p>
+      <div>
+        <Button href="/signup">Get Started</Button>
+      </div>
     </div>
   </div>
 );
