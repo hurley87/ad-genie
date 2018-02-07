32c4a126d0b8b9f74fc0ea807e5c3eb683a85c05
Carey Costa <careyxcosta@gmail.com>
2018-02-07 14:08:06 -0500

    added border-bottom to heading of About page

--- imports/ui/pages/About/About.js
+++ imports/ui/pages/About/About.js
@@ -6,15 +6,15 @@ const About = () => (
   <div className="About">
     <h3>How It Works</h3>
     <p>
-      <span className="list-num">1<span>
+      <span className="list-num">1. </span>
       Tell us about your <b>properties</b> and <b>ideal customers</b>
     </p>
     <p>
-      <span className="list-num">2</span>
+      <span className="list-num">2. </span>
       We engage in <b>conversations</b> with your <b>ideal customers</b>
     </p>
     <p>
-      <span className="list-num">3</span>
+      <span className="list-num">3. </span>
       They <b>respond with interest</b> and you <b>close deals</b>
     </p>
   </div>
