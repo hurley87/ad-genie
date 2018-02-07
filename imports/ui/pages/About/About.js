d15fca9bdcedf1f2e3e6518f4aa0d47e8b3d7fd4
Carey Costa <careyxcosta@gmail.com>
2018-02-07 13:48:15 -0500

    added className list-num to list numbers for specific styling

--- imports/ui/pages/About/About.js
+++ imports/ui/pages/About/About.js
@@ -5,9 +5,18 @@ import './About.scss';
 const About = () => (
   <div className="About">
     <h3>How It Works</h3>
-    <p>1. Tell us about your <b>properties</b> and <b>ideal customers</b></p>
-    <p>2. We engage in <b>conversations</b> with your <b>ideal customers</b></p>
-    <p>3. They <b>respond with interest</b> and you <b>close deals</b></p>
+    <p>
+      <span className="list-num">1<span>
+      Tell us about your <b>properties</b> and <b>ideal customers</b>
+    </p>
+    <p>
+      <span className="list-num">2</span>
+      We engage in <b>conversations</b> with your <b>ideal customers</b>
+    </p>
+    <p>
+      <span className="list-num">3</span>
+      They <b>respond with interest</b> and you <b>close deals</b>
+    </p>
   </div>
 );
 
