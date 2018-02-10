bb60f61928debae68fecb47b2ece7d0497850cf2
Carey Costa <careyxcosta@gmail.com>
2018-02-10 14:41:07 -0500

    updated pre-login/signup views: background, text alignment, and navigation

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -6,12 +6,12 @@ import './Index.scss';
 const Index = () => (
   <div className="Index">
     <div className="contentWrapper">
+    <img
+      src="https://s3.us-east-2.amazonaws.com/snapmortgages/cool.jpg"
+      alt="Clever Beagle"
+    />
       <h3>Prospecting is tedious, and inefficient.</h3>
-      <img
-        src="https://s3.us-east-2.amazonaws.com/snapmortgages/cool.jpg"
-        alt="Clever Beagle"
-      />
-      <p>We find your prospects, and drive qualified interest right to your inbox. You step in to close the deal.</p>
+      <p>We find your prospects, and drive qualified interest right to your inbox.<br/>You step in to close the deal.</p>
       <div>
         <Button href="/signup">Get Started</Button>
       </div>
